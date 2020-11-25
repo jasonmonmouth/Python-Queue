279b40bf87f91417cbd240f3d2a450d9035ea5b5
jasonmonmouth <72038820+jasonmonmouth@users.noreply.github.com>
2020-11-25 16:26:00 -0500

    queue simulation

---
+++ queue 12.py
@@ -0,0 +1,4 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> names = ["Jack", "James", "John", "Judy", "Jason", "Jake", "Jackie", "Joe", "Jody", "Jebediah"]names.append("James")names = ["Jack", "James", "John", "Judy", "Jason", "Jake", "Jackie", "Joe", "Jody", "Jebediah"]len(names) = (9)print (names.pop(3))
